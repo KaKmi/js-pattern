9e30ba0d499335d2bbbdb17c40bd833ea247d30d
赵超 <zhaochao@zhaochaodeMacBook-Pro.local>
2018-10-10 00:26:56 +0800

    decorator

--- decorator/index.ts
+++ decorator/index.ts
@@ -1,6 +1,20 @@
+class Plane {
+    constructor() {
 
-
-class ClassDecorator{
-
+    }
+    fire():void {
+        console.log('发射子弹')
+    }
 }
-export default new ClassDecorator()
+class AtomDecorator{
+    private plane:Plane
+    constructor(plane:Plane){
+        this.plane = plane
+    }
+    fire():void{
+        this.plane.fire();
+        console.log('发射原子弹')
+    }
+}
+
+export default {Plane,AtomDecorator}
